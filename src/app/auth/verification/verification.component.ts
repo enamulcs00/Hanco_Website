54e3f73eef556d8f0ae499995a021bd4ec20e3c4
unknown <suneet@apptunix.com>
2022-01-18 12:56:24 +0530

    update

--- src/app/auth/verification/verification.component.ts
+++ src/app/auth/verification/verification.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonDialogService } from 'src/app/common-data/common-dialog.service';
 
 @Component({
   selector: 'app-verification',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerificationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog,private commonData:CommonDialogService) { }
 
   ngOnInit(): void {
   }
+
+  profileSetup() {
+    this.dialog.closeAll();
+     this.commonData.profileSetup();
+    }
 
 }
