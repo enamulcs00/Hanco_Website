64421ec0794b5c83e1530b54ac2e16e5a755da26
Pawan <pawankumar@apptunix.com>
2022-04-07 20:02:38 +0530

    pawan 07/04/22

--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
+import {ThemePalette} from '@angular/material/core';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'app-home',
@@ -6,6 +15,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+ 
+  options: Options = {
+    floor: 0,
+    ceil: 250
+  };
+  minValue: number = 50;
+  maxValue: number = 200;
   slides = [
     {img: "assets/images/banner2.jpg"},
     {img: "assets/images/banner2.jpg"},
@@ -44,7 +60,37 @@ export class HomeComponent implements OnInit {
     
   }
 
- 
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      {name: 'Primary', completed: false, color: 'primary'},
+      {name: 'Accent', completed: false, color: 'accent'},
+      {name: 'Warn', completed: false, color: 'warn'},
+    ],
+  };
+
+  allComplete: boolean = false;
+  price:any=''
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => (t.completed = completed));
+  }
 
   afterChange(ev){
 this.currentSlide=ev.currentSlide
